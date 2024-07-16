e3b84cc03ffd0de351e5988d690af4d7e545e6cb
Tanner Davison <Tboydavison@gmail.com>
2024-07-15 22:25:16 -0600

    updating my learning lets go

--- Udemy_Course_Practice/SourceFiles/main.cpp
+++ Udemy_Course_Practice/SourceFiles/main.cpp
@@ -1,160 +1,98 @@
 #include <iostream>
 #include "Headers/Entities.h"
-
+#include <string>
 
 using namespace std;
-void inputScores(int& x, int& y, int& z) {
-	cout << "Input Scores per game: " << endl;
-	cin >> x >> y >> z;
+
+void inputArr(int* scoresP, int size) {
+	cout << "Input Scores: " << size << endl;
+	for (int i = 0; i < size; i++) {
+		cin >> scoresP[i];
+	}
+	cout << endl;
+}
+void printArr(int* scoresP, int size) {
+
+	for (int i = 0; i < size; i++) {
+		int show = i + 1;
+		cout << "#" << show << " " << scoresP[i] << endl;
+	}
 }
 
 
-
-float getAverageScore(const int& x, const int& y, const int& z) {
-
-	return (x + y + z) / 3.0f;
+int modifyValue(int* const ptr) {
+	static int changeVar = 0;
+	changeVar++;
+	return (*ptr) += changeVar;
 }
 
-template <typename MyAv>
-void print(const MyAv& value) {
-	cout << value;
+void updateScore(int* scoresP, int size) {
+	int numUpdate{};
+	int newVal{};
+	cout << "\nWhich number would you like to update?" << "\n";
+	cin >> numUpdate;
+	cout << "\ninput new value to replace" << "\n";
+	cin >> newVal;
+	(scoresP[numUpdate - 1]) = newVal;
+	cout << "Update Success" << "\n";
+	printArr(scoresP, size);
 }
+
+void verifyList(int* scoresP, int size) {
+	string command;
+	cout << "\nWould you like to make updates? (yes/stop)\n";
+	cin >> command;
+
+	while (command != "stop") {
+		if (command == "yes") {
+			updateScore(scoresP, size);
+		}
+		else {
+			cout << "Unknown command. Type 'yes' to update or 'stop' to exit.\n";
+		}
+		cout << "\nWould you like to make more updates? (yes/stop)\n";
+		cin >> command;
+	}
+
+	cout << "\nThanks! Have a great day!";
+}
+struct GreatestNum {
+
+	int score;
+
+};
+
+GreatestNum* getTheWinner(GreatestNum* stat1, GreatestNum* stat2) {
+	if (stat1->score > stat2->score) {
+		return stat1;
+	}
+	else if (stat2->score > stat1->score) {
+		return stat2;
+	}
+	else {
+		return nullptr;
+	}
+}
+
 
 int main() {
+	int size;
+	GreatestNum* stat1 = new GreatestNum{ 50 };
+	GreatestNum* stat2 = new GreatestNum{ 25 };
 
 
-	print(32);
+	cout << "How many scores would you like to track?\n";
+	cin >> size;
+	cout << endl;
 
-	//int demoArray[]{ 23,32,3333,6 };
-	//int* ptr{ demoArray };         // points to demoarray at index 0
-	//int& myRef{ *ptr };            // this is a reference to the pointer 
-	//// setting the reference that is referring to the pointer to reinitialize to 12
-	//int i = 0;
-	//cout << "DemoArray Size: " << sizeof(demoArray) << " Bytes" << endl;
-	//cout << "demoArray Memory: " << &demoArray << endl;
-
-	//cout << "\nPtr" << " Value: " << (*ptr) << endl;
-	//cout << "Size: " << sizeof(*ptr) << " Bytes" << endl;
-	//cout << "Memory Location: " << &ptr << endl;
-
-	//cout << "\nmyRef Var Value : " << myRef << " " << "\nSize: " << sizeof(myRef) << " Bytes" << endl;
-	//cout << "Memory Location : " << &myRef << endl;
-	//int q = 12;
-	//myRef = q;
-	//cout << "\nREF after being reinitialized  " << myRef << " Size: " << sizeof(myRef) << endl;
-	//cout << "Memory location of my ref: " << &myRef << endl;
-	//cout << "Changed value in demoArray[0] " << demoArray[0] << endl;
-
-	//int score1{}, score2{}, score3{};
-
-	//inputScores(score1, score2, score3);
-	//float averageScore = getAverageScore(score1, score2, score3);
-	//cout << "Youre average score was: " << averageScore << endl;
-
-	//Vector2D newLocation{ 32.2f, 44.2f };
-	//PlayerStats player1{ 100,32,0,0,200,10,2 };
-	//player1.setLocation(newLocation);
-	//player1.readStats();
-
-	//bool playing = true;
-	//int myNum = 23;
-	//PlayerStats player1{ 100, 50,0,0,200, 10, 1 }, player2{ 100,30,0,0,200,100,3 };
-	//player1.setLocation(122.2f, 300.3f);
-	//player2.setLocation(300.2f, 423.53f);
-	//PlayerStats players[] = { player1, player2 };
-
-	//AIStats rat{ 100, 25 }, alien{ 100, 50 };
-	//Entity entity{ 13.000 }, entity2{ 100.00 };
-	//rat = alien;
+	int* scoresP = new int[size];
 
 
-	//// POINTERS
-	//int* demopntr = nullptr;
-
-	//cout << demopntr << endl;
-	//demopntr = &myNum;
-	//cout << demopntr << endl;
-	//cout << *demopntr << endl;
-
-
-
-	/////////
-
-
-
-	//// Arrays Pointer example 
-
-	//int demoArray[]{ 1,5555555,3 , 45 };
-
-	//int* i{ demoArray };
-	//for (int temp : demoArray) {
-	//	cout << "START HERE ---" << endl;
-
-	//	cout << &i << endl;  //memory address of the pointer i;
-	//	cout << temp << endl; // close in memory to the pointer but is a reference 
-	//	cout << *i << endl; // dereferenced value of the pointer i;
-
-	//	i++;
-	//	cout << "----------------" << endl;
-	//}
-
-	/////evaluating equality in pointers;
-	//int x{ 5 }, * pntr{ &x }, * myPnt{ nullptr };
-
-	//myPnt = pntr;
-	//if (myPnt == pntr) { cout << *pntr << " == " << *myPnt << endl; };
-	//if (pntr != myPnt) { cout << *myPnt << " != " << *pntr << endl; }
-
-	////This works because player1.location is a Struct entity;
-	//printLocation(player1.location);
-	//int charCurrent{};
-	//int count{};
-
-	//cout << "Player 1: ( Enter: 1 )\nPlayer 2: ( Enter: 2 ) " << endl;
-	//cin >> charCurrent;
-	//do
-	//{
-	//	cout << "Player: " << charCurrent << endl;
-	//	cin.get();
-	//	cout << "Press enter to continue...";
-	//	cin.get();
-	//	string response;
-	//	if (count == 0) {
-	//		cout << "Do you want to play?" << endl;
-	//		cin >> response;
-	//	}
-	//	else if (count == 3) {
-	//		cout << "\n YOU WERE ATTACKED BY A DRAGON YOU FILTHY BASTARD" << endl;
-	//		cout << "\nYou lost 100 coins" << endl;
-	//		players[charCurrent - 1].xp -= 100;
-	//		cout << "\nDo you want to continue playing?" << endl;
-	//		cin >> response;
-	//		count = 0;
-	//	}
-	//	else {
-	//		cout << "Do you want to play Again?" << endl;
-	//		cin >> response;
-	//	}
-
-	//	if (response == "yes") {
-	//		cout << "It pays to play 10 XP for you" << endl;
-	//		players[charCurrent - 1].xp += 100;
-	//		count++;
-	//	}
-	//	else if (response == "no") {
-	//		playing = false;
-	//	}
-	//	else if (response == "stats") {
-	//		players[charCurrent - 1].readStats();
-	//	}
-	//} while (playing);
-	//if (playing == false) {
-	//	cout << "Thanks for playing!" << endl;
-	//}
-
-
-
-
-
+	inputArr(scoresP, size);
+	printArr(scoresP, size);
+	verifyList(scoresP, size);
+	GreatestNum* winner = getTheWinner(stat1, stat2);
+	cout << "\n\nAnd the winner is " << winner->score << endl;
+	delete[] scoresP;
 	return 0;
 }
---
+++ Udemy_Course_Practice/SourceFiles/notes.cpp
@@ -0,0 +1,158 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//int main() {
+//	//int demoArray[]{ 23,32,3333,6 };
+//		//int* ptr{ demoArray };         // points to demoarray at index 0
+//		//int& myRef{ *ptr };            // this is a reference to the pointer 
+//		//// setting the reference that is referring to the pointer to reinitialize to 12
+//		//int i = 0;
+//		//cout << "DemoArray Size: " << sizeof(demoArray) << " Bytes" << endl;
+//		//cout << "demoArray Memory: " << &demoArray << endl;
+//
+//		//cout << "\nPtr" << " Value: " << (*ptr) << endl;
+//		//cout << "Size: " << sizeof(*ptr) << " Bytes" << endl;
+//		//cout << "Memory Location: " << &ptr << endl;
+//
+//		//cout << "\nmyRef Var Value : " << myRef << " " << "\nSize: " << sizeof(myRef) << " Bytes" << endl;
+//		//cout << "Memory Location : " << &myRef << endl;
+//		//int q = 12;
+//		//myRef = q;
+//		//cout << "\nREF after being reinitialized  " << myRef << " Size: " << sizeof(myRef) << endl;
+//		//cout << "Memory location of my ref: " << &myRef << endl;
+//		//cout << "Changed value in demoArray[0] " << demoArray[0] << endl;
+//
+//		//int score1{}, score2{}, score3{};
+//
+//		//inputScores(score1, score2, score3);
+//		//float averageScore = getAverageScore(score1, score2, score3);
+//		//cout << "Youre average score was: " << averageScore << endl;
+//
+//		//Vector2D newLocation{ 32.2f, 44.2f };
+//		//PlayerStats player1{ 100,32,0,0,200,10,2 };
+//		//player1.setLocation(newLocation);
+//		//player1.readStats();
+//
+//		//bool playing = true;
+//		//int myNum = 23;
+//		//PlayerStats player1{ 100, 50,0,0,200, 10, 1 }, player2{ 100,30,0,0,200,100,3 };
+//		//player1.setLocation(122.2f, 300.3f);
+//		//player2.setLocation(300.2f, 423.53f);
+//		//PlayerStats players[] = { player1, player2 };
+//
+//		//AIStats rat{ 100, 25 }, alien{ 100, 50 };
+//		//Entity entity{ 13.000 }, entity2{ 100.00 };
+//		//rat = alien;
+//
+//
+//		//// POINTERS
+//		//int* demopntr = nullptr;
+//
+//		//cout << demopntr << endl;
+//		//demopntr = &myNum;
+//		//cout << demopntr << endl;
+//		//cout << *demopntr << endl;
+//
+//
+//
+//		/////////
+//
+//
+//
+//		//// Arrays Pointer example 
+//
+//		//int demoArray[]{ 1,5555555,3 , 45 };
+//
+//		//int* i{ demoArray };
+//		//for (int temp : demoArray) {
+//		//	cout << "START HERE ---" << endl;
+//
+//		//	cout << &i << endl;  //memory address of the pointer i;
+//		//	cout << temp << endl; // close in memory to the pointer but is a reference 
+//		//	cout << *i << endl; // dereferenced value of the pointer i;
+//
+//		//	i++;
+//		//	cout << "----------------" << endl;
+//		//}
+//
+//		/////evaluating equality in pointers;
+//		//int x{ 5 }, * pntr{ &x }, * myPnt{ nullptr };
+//
+//		//myPnt = pntr;
+//		//if (myPnt == pntr) { cout << *pntr << " == " << *myPnt << endl; };
+//		//if (pntr != myPnt) { cout << *myPnt << " != " << *pntr << endl; }
+//
+//		////This works because player1.location is a Struct entity;
+//		//printLocation(player1.location);
+//		//int charCurrent{};
+//		//int count{};
+//
+//		//cout << "Player 1: ( Enter: 1 )\nPlayer 2: ( Enter: 2 ) " << endl;
+//		//cin >> charCurrent;
+//		//do
+//		//{
+//		//	cout << "Player: " << charCurrent << endl;
+//		//	cin.get();
+//		//	cout << "Press enter to continue...";
+//		//	cin.get();
+//		//	string response;
+//		//	if (count == 0) {
+//		//		cout << "Do you want to play?" << endl;
+//		//		cin >> response;
+//		//	}
+//		//	else if (count == 3) {
+//		//		cout << "\n YOU WERE ATTACKED BY A DRAGON YOU FILTHY BASTARD" << endl;
+//		//		cout << "\nYou lost 100 coins" << endl;
+//		//		players[charCurrent - 1].xp -= 100;
+//		//		cout << "\nDo you want to continue playing?" << endl;
+//		//		cin >> response;
+//		//		count = 0;
+//		//	}
+//		//	else {
+//		//		cout << "Do you want to play Again?" << endl;
+//		//		cin >> response;
+//		//	}
+//
+//		//	if (response == "yes") {
+//		//		cout << "It pays to play 10 XP for you" << endl;
+//		//		players[charCurrent - 1].xp += 100;
+//		//		count++;
+//		//	}
+//		//	else if (response == "no") {
+//		//		playing = false;
+//		//	}
+//		//	else if (response == "stats") {
+//		//		players[charCurrent - 1].readStats();
+//		//	}
+//		//} while (playing);
+//		//if (playing == false) {
+//		//	cout << "Thanks for playing!" << endl;
+//		//}
+// 
+// 
+// // DYNAMIC MEMORY ALLOCATION FOR ARRAYS 
+// int arraySize{};
+//cout << "Enter how any students are in the class" << "\n";
+//cin >> arraySize;
+//
+//int* myArrayPtr = new int[arraySize];
+//
+//cout << "enter the " << arraySize << "scores: \n";
+//for (int i = 0; i < arraySize; i++) {
+//	cin >> myArrayPtr[i];
+//}
+//
+//cout << "the scores are: ";
+//for (int i = 0; i < arraySize; i++) {
+//	cout << myArrayPtr[i] << "\n";
+//
+//}
+//
+//cout << endl;
+//
+//delete[] myArrayPtr;
+//myArrayPtr = nullptr;
+
+//}
